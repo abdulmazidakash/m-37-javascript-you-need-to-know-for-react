e58a0b9d45785542ca534dd67f79a51fdadd4b99
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-10-15 16:51:42 +0600

    37-7

---
+++ storage.js
@@ -0,0 +1,18 @@
+// localStorage.setItem('userId', 543453)
+
+const addToLocalStorage = ()=>{
+	const idInput = document.getElementById('storage-id');
+	const id = idInput.value ;
+	const valueInput = document.getElementById('storage-value');
+	const value = valueInput.value ;
+
+	//
+	if(id && value){
+
+		localStorage.setItem(id, value);
+	}
+	idInput.value = '';
+	valueInput.value = '';
+}
+
+// addToLocalStorage()
